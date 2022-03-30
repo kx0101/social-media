e9d80c1283229bd7ef7687a6b1f8ea8d49548e1a
liakos.koulaxis@yahoo.com <liakos.koulaxis@yahoo.com>
2022-03-30 14:59:52 +0300

    file upload logic

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
-const cors = require("cors")
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const dirPath = path.join(__dirname, 'public/images');
 
 // Initiating app
 const app = express();
@@ -20,10 +23,32 @@ mongoose.connect(process.env.MONGO_URL, () => {
     console.log("Connected to MongoDB Cluster");
 });
 
+app.use("/public/images", express.static(path.join(dirPath)))
+
 // Middleware
 app.use(express.json()); //bodyParser for POST requests
 app.use(helmet());
-app.use(morgan("common"));
+app.use(morgan("common"));  
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        const d = new Date();
+        let minutes = d.getMinutes();
+        cb(null, minutes + file.originalname)
+    }
+})
+
+const upload = multer({storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+        return res.status(200).json("File uploaded successfully.")
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 // Routes
 app.use("/api/users", userRoute);
